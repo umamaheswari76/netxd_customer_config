4475f816eab85cf2c76bc129b413290d3e3b278f
umamaheswari76 <umamahes1301@gmail.com>
2023-08-31 11:26:08 +0530

    added config folders

---
+++ constants/index.go
@@ -0,0 +1,7 @@
+// define all the constants that we will use in the application.
+
+package constants
+
+const ConnectionString ="mongodb://localhost:27017/?retryWrites=true&connectTimeoutMS=10000"
+const Port =":5001"
+const DatabaseName ="netxd_project"
